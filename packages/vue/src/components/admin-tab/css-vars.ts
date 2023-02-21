5bea224c59a00ec93649a8f2a74d685980bfa65a
Soybean <honghuangdc@gmail.com>
2023-02-21 08:14:25 +0800

    feat(projects): add chrome tab bg color

--- packages/vue/src/components/admin-tab/css-vars.ts
+++ packages/vue/src/components/admin-tab/css-vars.ts
@@ -4,6 +4,8 @@ type Prefix = '--soy-';
 
 export type CssVarsProps = {
   primaryColor: string;
+  primaryColor1: string;
+  primaryColor2: string;
   primaryColorOpacity1: string;
   primaryColorOpacity2: string;
   primaryColorOpacity3: string;
@@ -16,6 +18,8 @@ type CssVars = {
 export function createCssVars(props: CssVarsProps) {
   const cssVars: CssVars = {
     '--soy-primary-color': props.primaryColor,
+    '--soy-primary-color1': props.primaryColor1,
+    '--soy-primary-color2': props.primaryColor2,
     '--soy-primary-color-opacity1': props.primaryColorOpacity1,
     '--soy-primary-color-opacity2': props.primaryColorOpacity2,
     '--soy-primary-color-opacity3': props.primaryColorOpacity3
