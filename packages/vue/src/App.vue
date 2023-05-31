97b74bc1dcea4762f98fd89687c5032dfb26494d
Soybean <honghuangdc@gmail.com>
2023-05-31 22:43:48 +0800

    feat(projects): add demo show switch

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <!-- <PageTabDemo /> -->
-    <AdminLayoutDemo />
+    <PageTabDemo v-if="showTab" />
+    <AdminLayoutDemo v-else />
   </div>
 </template>
 
 <script setup lang="ts">
 import AdminLayoutDemo from './demos/admin-layout.vue';
-// import PageTabDemo from './demos/page-tab.vue';
+import PageTabDemo from './demos/page-tab.vue';
 
 defineOptions({
   name: 'App'
 });
+const showTab = true;
 </script>
 
 <style scoped></style>
